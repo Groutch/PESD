46d8b99fb7752789958a108788fb51522f2dd1fc
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-07-13 09:17:55 +0200

    some server tests

---
+++ test/test.server.js
@@ -0,0 +1,41 @@
+const request = require('supertest');
+const app = require('../server.js');
+const should = require('should');
+
+
+describe('GET /signup' , ()=>{
+	it('should return signup page', (done)=>{
+		request(app)
+		.get('/signup')
+		.end((err, res) => {
+			should.exist(res);
+			res.status.should.be.equal(200);
+			done();
+		});
+	});
+});
+
+describe('GET /' , ()=>{
+	it('should return homepage', (done)=>{
+		request(app)
+		.get('/')
+		.end((err, res) => {
+			should.exist(res);
+			res.status.should.be.equal(200);
+			done();
+		});
+	});
+});
+
+describe('GET /dashboard-c' , ()=>{
+	it('should return dashboard_candidate page', (done)=>{
+		request(app)
+		.get('/dashboard-c')
+		.end((err, res) => {
+			should.exist(res);
+			res.status.should.be.equal(200);
+			done();
+		});
+	});
+});
+
