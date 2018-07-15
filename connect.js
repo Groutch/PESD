06971cc0e27bca11300f20519b1675ce30dd8ba9
Valentin Janvier <alencruserxx@hotmail.fr>
2018-07-15 12:41:34 +0200

    index et fichier connect

---
+++ connect.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+let connection = mysql.createConnection({
+    multipleStatements: true, // To make multiple mysql queries
+    host: 'den1.mysql6.gear.host',
+    user: 'plaisir',
+    password: 'Ht33_oz8j_3z',
+    database: 'plaisir'
+});
+
+module.exports = connection;
